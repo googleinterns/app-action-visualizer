dde97efc662a3656de6302928713631736c0a8ab
Hua Chen <chhua@google.com>
2020-07-07 17:49:52 -0700

    update the action page with two kinds of view type for better view, add the parameter activity

---
+++ AppActionVisualizer/app/src/main/java/com/example/appactionvisualizer/ui/activity/ParameterActivity.java
@@ -0,0 +1,13 @@
+package com.example.appactionvisualizer.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParameterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parameter);
+    }
+}
