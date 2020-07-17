027dfbabc54cc5f195eb61b3a583d69c0a7c1204
Hua Chen <chhua@google.com>
2020-07-16 17:13:16 -0700

    add select location page

--- AppActionVisualizer/app/src/main/java/com/example/appactionvisualizer/ui/activity/parameter/ListItemActivity.java
+++ AppActionVisualizer/app/src/main/java/com/example/appactionvisualizer/ui/activity/parameter/ListItemActivity.java
@@ -1,16 +1,18 @@
-package com.example.appactionvisualizer.ui.activity;
+package com.example.appactionvisualizer.ui.activity.parameter;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appactionvisualizer.R;
 import com.example.appactionvisualizer.constants.Constant;
 import com.example.appactionvisualizer.databean.ParameterMapping;
+import com.example.appactionvisualizer.ui.activity.CustomActivity;
 import com.example.appactionvisualizer.ui.adapter.SelectRecyclerViewAdapter;
 
 public class SelectActivity extends CustomActivity {
@@ -18,7 +20,7 @@ public class SelectActivity extends CustomActivity {
   private String key;
 
   @Override
-  void initData() {
+  protected void initData() {
     Intent intent = getIntent();
     parameterMapping = (ParameterMapping) intent.getSerializableExtra(Constant.PARAMETER_MAPPING);
     key = intent.getStringExtra(Constant.KEY);
@@ -33,16 +35,19 @@ public class SelectActivity extends CustomActivity {
   }
 
   @Override
-  void initView() {
+  protected void initView() {
     super.initView();
     getSupportActionBar().setTitle(key);
     RecyclerView view = findViewById(R.id.list);
     // Set the adapter
-    if (view instanceof RecyclerView) {
+    if (view != null) {
       Context context = view.getContext();
       RecyclerView recyclerView = view;
       recyclerView.setLayoutManager(new LinearLayoutManager(context));
       recyclerView.setAdapter(new SelectRecyclerViewAdapter(parameterMapping.getKey2MapList().get(key), key, this));
+      DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+          new LinearLayoutManager(SelectActivity.this).getOrientation());
+      recyclerView.addItemDecoration(dividerItemDecoration);
     }
   }
 
